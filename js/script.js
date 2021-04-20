f0f9ce242dfc11065838bd8864670ab9075e92e5
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-20 15:53:19 +0200

    javascript completo, bottoni non dinamici

--- js/script.js
+++ js/script.js
@@ -2,30 +2,46 @@ $(document).ready(function () {
   //references
   var anglePrev = $(".vg-prev");
   var angleNext = $(".vg-next");
+
   console.log(angleNext);
 
   //events
-  /*   anglePrev.click(prevImage);
-   */
+  anglePrev.click(prevImage);
+
   angleNext.click(nextImage);
 
   //functions
 
-  /* function prevImage() {
+  function prevImage() {
+    var activeImage = $(".wrapper-images img.active");
+    var activeCircle = $(".wrapper-buttons .vg-circle.active");
+    console.log(activeCircle);
+    activeImage.removeClass("active");
+    activeCircle.removeClass("active");
 
-  } */
+    if (activeImage.prev("img").length === 0) {
+      $(".wrapper-images img").last().addClass("active");
+      $(".wrapper-buttons .vg-circle").last().addClass("active");
+    } else {
+      activeImage.prev("img").addClass("active");
+      activeCircle.prev(".vg-circle").addClass("active");
+    }
+  }
 
   function nextImage() {
     //dichiarazione variabili
     var activeImage = $(".wrapper-images img.active"); //immagine con display:block
+    var activeCircle = $(".wrapper-buttons .vg-circle.active");
+
     activeImage.removeClass("active");
-    console.log(activeImage);
+    activeCircle.removeClass("active");
 
     if (activeImage.next("img").length === 0) {
       $(".wrapper-images img").first().addClass("active");
+      $(".wrapper-buttons .vg-circle").first().addClass("active");
     } else {
       activeImage.next("img").addClass("active");
-      console.log(activeImage.next("img"));
+      activeCircle.next(".vg-circle").addClass("active");
     }
   }
 });
